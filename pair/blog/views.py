2e4ab3cffa1d6bd82faddbb19647c73bf5161a9f
BestShes <qkrdngus16@gmail.com>
2017-02-08 12:04:54 +0900

    post_detail 생성 및 구현 post_list의 리스트값이 없을경우 구현

--- pair/blog/views.py
+++ pair/blog/views.py
@@ -7,8 +7,8 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    post = Post.objects.all().order_by('-created_date')
 
+    post = Post.objects.all().order_by('-created_date')
 
     context = {
         'post_list': post
@@ -16,3 +16,10 @@ def post_list(request):
     }
 
     return render(request, 'blog/post-list.html', context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {
+        'post_detail': post
+    }
+    return render(request, 'blog/post-detail.html', context)
