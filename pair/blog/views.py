11971fe693b21ec27fad05a1fd8afecd48103a90
BestShes <qkrdngus16@gmail.com>
2017-02-08 11:48:35 +0900

    포스트 list views ,  html 작성

--- pair/blog/views.py
+++ pair/blog/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post
+
+
 # Create your views here.
+
+def post_list(request):
+    post = Post.objects.all().order_by('-created_date')
+
+
+    context = {
+        'post_list': post
+
+    }
+
+    return render(request, 'blog/post-list.html', context)
